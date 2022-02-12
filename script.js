bde5e5f2a2c389998a7aca0847d51385b91e6e08
KateSerhiienko <kate.serhiienko@gmail.com>
2022-02-12 17:32:37 +0200

    fix(script):validation

--- script.js
+++ script.js
@@ -12,25 +12,25 @@ function showData(e) {
       e.target[i].checked ? e.target[i].value = true : e.target[i].value = false;
     }
     formData[e.target[i].name] = e.target[i].value;
+  }
 
-
-    if (validateForm(formData)) {
-      console.log(e.target[i].name + "->" + e.target[i].value);
-    }
+  if (validateForm(formData)) {
+    // console.log(e.target[i].name + "->" + e.target[i].value);
+    console.log(1);
   }
 }
 
 function validateForm(formData) {
   let isformValid = true;
 
-  if (firstName.length < 2) {
+  if (formData.firstName.length < 2) {
     isformValid = false;
     highlightField(firstName);
   } else {
     unhighlightField(firstName)
   }
 
-  if (lastName.length < 2) {
+  if (formData.lastName.length < 2) {
     isformValid = false;
     highlightField(lastName);
   } else {
@@ -38,14 +38,14 @@ function validateForm(formData) {
   }
 
   const emailRegexp = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-  if (emailRegexp.test(email)) {
+  if (!emailRegexp.test(formData.email)) {
     isformValid = false;
     highlightField(email);
   } else {
     unhighlightField(email)
   }
 
-  if (typeof phoneNumber != Number && phoneNumber.length < 6 || phoneNumber.length > 12) {
+  if (typeof formData.phoneNumber != Number && formData.phoneNumber.length < 6 || formData.phoneNumber.length > 12) {
     isformValid = false;
     highlightField(phoneNumber);
   } else {
