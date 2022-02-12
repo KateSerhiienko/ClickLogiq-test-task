2d22b693234ce422009e66d6a92533a717b63920
KateSerhiienko <kate.serhiienko@gmail.com>
2022-02-12 17:25:36 +0200

    add(js):validation

--- script.js
+++ script.js
@@ -1,18 +1,71 @@
 const form = document.querySelector("#form");
 
-// let formData = new FormData(form);
-
-// console.log(form.value);
-
-// const emailRegexp = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-// emailRegexp.test(this.email) ? this.emailValid = true : this.emailValid = false;
-
 function showData(e) {
   e.preventDefault();
+
   const formData = {};
-  for (let i = 0; i < e.target.length; i++)
+
+  for (let i = 0; i < e.target.length; i++) {
+
+    if (e.target[i].name === 'submit') continue;
+    if (e.target[i].name === 'agree') {
+      e.target[i].checked ? e.target[i].value = true : e.target[i].value = false;
+    }
     formData[e.target[i].name] = e.target[i].value;
-  console.log(formData);
+
+
+    if (validateForm(formData)) {
+      console.log(e.target[i].name + "->" + e.target[i].value);
+    }
+  }
+}
+
+function validateForm(formData) {
+  let isformValid = true;
+
+  if (firstName.length < 2) {
+    isformValid = false;
+    highlightField(firstName);
+  } else {
+    unhighlightField(firstName)
+  }
+
+  if (lastName.length < 2) {
+    isformValid = false;
+    highlightField(lastName);
+  } else {
+    unhighlightField(lastName)
+  }
+
+  const emailRegexp = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+  if (emailRegexp.test(email)) {
+    isformValid = false;
+    highlightField(email);
+  } else {
+    unhighlightField(email)
+  }
+
+  if (typeof phoneNumber != Number && phoneNumber.length < 6 || phoneNumber.length > 12) {
+    isformValid = false;
+    highlightField(phoneNumber);
+  } else {
+    unhighlightField(phoneNumber)
+  }
+
+  return isformValid;
+}
+
+function highlightField(field) {
+  field.classList.add("invalid");
+}
+function unhighlightField(field) {
+  field.classList.remove("invalid");
 }
 
 form.addEventListener('submit', showData);
+
+form.addEventListener('keydown', function (event) {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+  }
+}); // disable submit by enter button
